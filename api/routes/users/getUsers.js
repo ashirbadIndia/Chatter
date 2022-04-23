87db45fbe1cd774f699da15697d5407fd8d18cd0
ashirbadIndia <ashirbad.behera.india@gmail.com>
2022-04-23 18:49:08 +0530

    improve search

--- api/routes/users/getUsers.js
+++ api/routes/users/getUsers.js
@@ -2,7 +2,25 @@ const User = require('../../models/user');
 
 module.exports = async (req,res,next)=>{
     try{
-        const users = await User.find(req.query).exec();
+        const terms = req.query.term.split(" ").filter((ele)=>ele);
+        
+        let query = [ {
+            firstName: {$regex: terms[0] , $options:'i'}
+        }, {
+            lastName: {$regex: terms[0] , $options:'i'}
+        },{
+            emailId: {$regex: terms[0] , $options:'i'}
+        } ];
+
+        if(terms.length == 2){
+            query = [{
+                firstName: {$regex: terms[0] , $options:'i'},
+                lastName: {$regex: terms[1] , $options:'i'}
+            },...query]
+            console.log(query);
+        }
+
+        const users = await User.find({ $or:  query}).exec();
         const response = users.map((user)=>{
             return {
                 firstName: user.firstName,
